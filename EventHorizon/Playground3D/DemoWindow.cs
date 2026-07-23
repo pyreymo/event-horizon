7ef1eb51f821532cfc18fec2a4c04d554a26976e
pyreymo <pyreymo@gmail.com>
2026-07-23 20:29:08 +0800

    Return renderer to the last boot-stable baseline

--- EventHorizon/Playground3D/DemoWindow.cs
+++ EventHorizon/Playground3D/DemoWindow.cs
@@ -1,36 +1,14 @@
 using System.Numerics;
 using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.Windowing;
-using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
-using Underpaint;
 
 namespace EventHorizon.Playground3D;
 
-internal sealed unsafe class DemoWindow : Window
+internal sealed class DemoWindow : Window
 {
-    private const ulong TriangleId = 1;
-    private const ulong QuadId = 3;
-    private const int MainView = 30;
-    private const int MainRenderCameraSubView = 12;
-
-    private readonly Renderer? renderer;
-    private readonly Primitive[] frame = new Primitive[3];
-    private Matrix4x4 anchorWorld;
-    private Matrix4x4 currentWorld;
-    private Matrix4x4 secondCurrentWorld;
-    private Matrix4x4 quadCurrentWorld;
-    private bool hasAnchorWorld;
-    private Vector3 offset;
-    private float rotationDegrees;
-    private float scale = 1;
-    private Vector3 color = new(1, 0, 0);
-    private float alpha = 0.5f;
-    private float ditherFade = 1;
-
-    public DemoWindow(Renderer? renderer)
+    public DemoWindow()
         : base("Rendering Research")
     {
-        this.renderer = renderer;
         IsOpen = false;
         SizeCondition = ImGuiCond.FirstUseEver;
         Size = new Vector2(560, 480);
@@ -43,66 +21,5 @@ internal sealed unsafe class DemoWindow : Window
 
     public void Dispose() { }
 
-    public void SubmitFrame()
-    {
-        if (renderer == null)
-            return;
-
-        if (!hasAnchorWorld)
-        {
-            var manager = Manager.Instance();
-            var camera = manager == null ? null : manager->Views[MainView].SubViews[MainRenderCameraSubView].Camera;
-            if (camera == null)
-                return;
-
-            var view = (Matrix4x4)camera->ViewMatrix;
-            view.M44 = 1;
-            if (!Matrix4x4.Invert(view, out var inverseView))
-                return;
-
-            anchorWorld = Matrix4x4.CreateTranslation(0, 0, -5) * inverseView;
-            currentWorld = anchorWorld;
-            secondCurrentWorld = Matrix4x4.CreateTranslation(1.5f, 0, 0) * anchorWorld;
-            quadCurrentWorld = Matrix4x4.CreateTranslation(-1.5f, 0, 0) * anchorWorld;
-            hasAnchorWorld = true;
-        }
-
-        var previousWorld = currentWorld;
-        var secondPreviousWorld = secondCurrentWorld;
-        var quadPreviousWorld = quadCurrentWorld;
-        currentWorld =
-            Matrix4x4.CreateScale(scale)
-            * Matrix4x4.CreateRotationY(rotationDegrees * MathF.PI / 180)
-            * Matrix4x4.CreateTranslation(offset)
-            * anchorWorld;
-        secondCurrentWorld = Matrix4x4.CreateScale(0.75f) * Matrix4x4.CreateTranslation(offset + new Vector3(1.5f, 0, 0)) * anchorWorld;
-        quadCurrentWorld = Matrix4x4.CreateScale(0.75f) * Matrix4x4.CreateTranslation(offset + new Vector3(-1.5f, 0, 0)) * anchorWorld;
-
-        frame[0] = new Primitive(PrimitiveType.Triangle, TriangleId, currentWorld, previousWorld, color, alpha, ditherFade);
-        frame[1] = new Primitive(PrimitiveType.Triangle, 2, secondCurrentWorld, secondPreviousWorld, new Vector3(0, 1, 0), 0.75f, 1);
-        frame[2] = new Primitive(PrimitiveType.Quad, QuadId, quadCurrentWorld, quadPreviousWorld, new Vector3(0, 0.5f, 1), 0.65f, 1);
-        renderer.SubmitFrame(frame);
-    }
-
-    public override void Draw()
-    {
-        ImGui.DragFloat3("Offset", ref offset, 0.05f);
-        ImGui.SliderFloat("Rotation", ref rotationDegrees, -180, 180, "%.0f deg");
-        ImGui.SliderFloat("Scale", ref scale, 0.1f, 5, "%.2f");
-        ImGui.ColorEdit3("Color", ref color);
-        ImGui.SliderFloat("Alpha", ref alpha, 0, 1, "%.2f");
-        ImGui.SliderFloat("Dither fade (unknown semantics)", ref ditherFade, 0, 1, "%.2f");
-        ImGui.TextUnformatted("Triangle 2: fixed green, alpha 0.75, shifted right.");
-        ImGui.TextUnformatted("Quad: fixed blue, alpha 0.65, shifted left.");
-
-        if (ImGui.Button("Reset"))
-        {
-            offset = default;
-            rotationDegrees = 0;
-            scale = 1;
-            color = new Vector3(1, 0, 0);
-            alpha = 0.5f;
-            ditherFade = 1;
-        }
-    }
+    public override void Draw() { }
 }
--- EventHorizon/Plugin.cs
+++ EventHorizon/Plugin.cs
@@ -175,7 +175,7 @@ public sealed class Plugin : IDalamudPlugin
             );
             ConfigWindow = new ConfigWindow(this, DataManager, playerPreviewPanel, IsPlayerPreviewWindowOpen, TogglePlayerPreviewWindow);
             PlayerPreviewWindow = new PlayerPreviewWindow(playerPreviewPanel, OpenMainUi);
-            UnderpaintDemoWindow = new DemoWindow(UnderpaintRenderer);
+            UnderpaintDemoWindow = new DemoWindow();
             DtrStatusBar = new DtrBar(DtrBar, Configuration, Culling.GetStatus, SetPlayerHidingEnabled, ToggleConfigUi);
             DtrBackground = new DtrBackground(AddonLifecycle, GameGui, Framework, ClientState, Configuration);
             TargetingMarkerController = new TargetingMarkerController(
@@ -397,7 +397,6 @@ public sealed class Plugin : IDalamudPlugin
 
     private void OnFrameworkUpdate(IFramework framework)
     {
-        UnderpaintDemoWindow.SubmitFrame();
         Culling.TemporarilyShowAllPlayers = IsTemporaryShowAllPlayersShortcutHeld();
 
         SceneVisibilityController.Update();
